da9faf9260ae473f92107ecba1ff302ac2a99b20
Alan <cloojure@gmail.com>
2022-01-31 15:34:55 -0800

    misc

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.10.3"]
-                 [prismatic/schema "1.1.12"]
+                 [prismatic/schema "1.2.0"]
                  [tupelo "22.01.31"]
                  ]
   :plugins [
             [com.jakemccrary/lein-test-refresh "0.25.0"]
-            [lein-ancient "0.6.15"]
+            [lein-ancient "0.7.0"]
             ]
 
   :db "jdbc:postgresql://localhost/default"
